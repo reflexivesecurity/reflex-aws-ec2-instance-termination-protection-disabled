8235c1fc1eb8adbc91bad8885d896dc6808ad9d6
rjulian <richard@rjulian.net>
2020-05-17 14:23:19 -0700

    Multiregion refactor

---
+++ terraform/sqs_lambda/sqs_lambda.tf
@@ -0,0 +1,24 @@
+module "sqs_lambda" {
+  source = "git::https://github.com/cloudmitigator/reflex-engine.git//modules/sqs_lambda?ref=v0.6.0"
+
+  cloudwatch_event_rule_id  = var.cloudwatch_event_rule_id
+  cloudwatch_event_rule_arn = var.cloudwatch_event_rule_arn
+  function_name             = "Ec2InstanceTerminationProtectionDisabled"
+  source_code_dir           = "${path.module}/../../source"
+  handler                   = "reflex_aws_ec2_instance_termination_protection_disabled.lambda_handler"
+  lambda_runtime            = "python3.7"
+  environment_variable_map = {
+    SNS_TOPIC = var.sns_topic_arn,
+
+  }
+
+
+
+  queue_name    = "Ec2InstanceTerminationProtectionDisabled"
+  delay_seconds = 0
+
+  target_id = "Ec2InstanceTerminationProtectionDisabled"
+
+  sns_topic_arn  = var.sns_topic_arn
+  sqs_kms_key_id = var.reflex_kms_key_id
+}
