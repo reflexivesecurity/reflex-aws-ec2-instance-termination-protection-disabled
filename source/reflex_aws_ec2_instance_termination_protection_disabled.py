494f24612e4bdfe0afdeeee40e7fb444f6bd55dc
rjulian <richard@rjulian.net>
2020-09-13 14:46:41 -0700

    Semi-automated multi account push

--- source/reflex_aws_ec2_instance_termination_protection_disabled.py
+++ source/reflex_aws_ec2_instance_termination_protection_disabled.py
@@ -4,7 +4,7 @@ import json
 import os
 
 import boto3
-from reflex_core import AWSRule
+from reflex_core import AWSRule, subscription_confirmation
 
 
 class InstanceTerminationProtectionDisabled(AWSRule):
@@ -34,5 +34,9 @@ class InstanceTerminationProtectionDisabled(AWSRule):
 
 def lambda_handler(event, _):
     """ Handles the incoming event """
+    print(event)
+    if subscription_confirmation.is_subscription_confirmation(event):
+        subscription_confirmation.confirm_subscription(event)
+        return
     rule = InstanceTerminationProtectionDisabled(json.loads(event["Records"][0]["body"]))
     rule.run_compliance_rule()
